19d220818748f4909b6aef64d6789c0509881ed2
Mathieu Desjarlais <github@desmat.ca>
2025-08-12 16:18:04 -0400

    rm layout, simple index

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,25 @@
 <template>
-  <h1>index page</h1>
-  <h2>no auth required</h2>
-  <RouterLink to="loggedin">To logged in page</RouterLink>
+  <div style="padding: 1rem; text-align: center">
+    <h1>index page</h1>
+    <h2>auth required</h2>
+    
+    <div style="display: flex; flex-direction: row; gap: 1rem; justify-content: center;">
+      <RouterLink to="/">index page</RouterLink>
+      <RouterLink to="login">login page</RouterLink>
+      <a :href="loginUrl">scbd loginUrl</a>
+      <a :href="logoutUrl">scbd logoutUrl</a>
+      <a :href="profileUrl">scbd profileUrl</a>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-console.log('index.vue load')
-
+<script
+  setup
+  lang="ts"
+>
 definePageMeta({
-  auth: false,
+  roles: [Authenticated],
 });
+
+const { loginUrl, logoutUrl, profileUrl } = useScbdAuthConfig();
 </script>
